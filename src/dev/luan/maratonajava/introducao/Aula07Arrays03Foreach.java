3a06124765873fdfe046dd7f307ba096f32be730
Luan Araújo <luan-mateusss@hotmail.com>
2025-05-23 13:31:04 -0300

    Initial commit

--- src/dev/luan/maratonajava/introducao/Aula07Arrays03Foreach.java
+++ src/dev/luan/maratonajava/introducao/Aula07Arrays03Foreach.java
@@ -3,13 +3,13 @@ package dev.luan.maratonajava.introducao;
 public class Aula07Arrays03Foreach {
     public static void main(String[] args) {
         //int[] numeros = new int[10]; // Formas de declarar Arrays
-        //int[] numeros2  = {1,2,3,4,5};
+        int[] numeros2  = {1,2,3,4,5};
         int[] numeros3  = new int[]{5,4,3,2,1};
 
         //For
-        /*for(int i = 0; i < numeros2.length; i++) {
+        for(int i = 0; i < numeros2.length; i++) {
             System.out.println(numeros2[i]);
-        }*/
+        }
         System.out.println();
 
         //Foreach - Esse for só tem como fazer a leitura completa, não tem como acessar o indice que desejar. Essa é uma versão simplificada.
