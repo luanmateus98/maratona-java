2a53d80efff48be0c7b000f0710b106392605787
Luan Araújo <luan-mateusss@hotmail.com>
2025-05-23 13:28:49 -0300

    Initial commit

--- src/dev/luan/maratonajava/introducao/Aula07Arrays03Foreach.java
+++ src/dev/luan/maratonajava/introducao/Aula07Arrays03Foreach.java
@@ -2,7 +2,7 @@ package dev.luan.maratonajava.introducao;
 
 public class Aula07Arrays03Foreach {
     public static void main(String[] args) {
-        int[] numeros = new int[10]; // Formas de declarar Arrays
+        //int[] numeros = new int[10]; // Formas de declarar Arrays
         int[] numeros2  = {1,2,3,4,5};
         int[] numeros3  = new int[]{5,4,3,2,1};
 
